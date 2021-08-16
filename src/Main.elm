042620da2757f6c01709ec3bb181de1200b36825
Duy Khang <khangemail22@gmail.com>
2021-08-16 21:01:07 +0700

    Add content

--- src/Main.elm
+++ src/Main.elm
@@ -3,17 +3,19 @@ module Main exposing (main)
 import Array exposing (Array)
 import Browser
 import Converter exposing (timetableToEvents)
+import Css
 import Event exposing (Event)
 import Html.Styled as Html exposing (Html, toUnstyled)
-import Html.Styled.Attributes exposing (download, href, target, value)
+import Html.Styled.Attributes exposing (css, download, for, href, id, name, target, value)
 import Html.Styled.Events exposing (onClick, onInput)
+import Markdown
 import Timetable exposing (Timetable)
 import Url exposing (percentEncode)
 
 
 type alias Model =
     { raw : String
-    , result : Maybe Timetable
+    , readyToDownload : Bool
     , events : Array Event
     }
 
@@ -31,7 +33,7 @@ main =
 init : flags -> ( Model, Cmd msg )
 init _ =
     ( { raw = ""
-      , result = Nothing
+      , readyToDownload = False
       , events = Array.empty
       }
     , Cmd.none
@@ -40,158 +42,110 @@ init _ =
 
 view : Model -> Html Msg
 view model =
-    Html.div []
-        [ Html.textarea [ onInput GotInput, value model.raw ] []
-        , case model.result of
-            Nothing ->
-                Html.text ""
+    Html.div [ css [ Css.maxWidth (Css.px 500), Css.margin Css.auto]]
+        [ Html.h1 [] [ Html.text "BKalendar" ]
+        , Html.label [ for "timetable-input" ]
+            [ Html.text "Copy thời khóa biểu vào đây" ]
+        , Html.textarea
+            [ css [ Css.display Css.block ]
+            , id "timetable-input"
+            , name "timetable-input"
+            , onInput GotInput
+            , value model.raw
+            ]
+            []
+        , if not model.readyToDownload then
+            Html.text ""
 
-            Just timetable ->
-                Html.div []
-                    [ viewTimetable timetable
-                    , if List.length timetable.entries /= Array.length model.events then
-                        Html.text ""
-
-                      else
-                        viewEvents model.events
-                    ]
+          else
+            viewDownload model.events
+        , viewGuide
         ]
 
 
-viewEvents : Array Event -> Html Msg
-viewEvents events =
+viewGuide : Html Msg
+viewGuide =
+    Html.div []
+        [ Html.fromUnstyled <| Markdown.toHtml [] """
+## Tạo lịch mới trên Google Calendar
+
+1. Mở Google Calendar trên máy tính.
+2. Ở thanh bên trái, kéo xuống, cạnh phần **Lịch khác**, hãy nhấp vào biểu tượng **Thêm lịch khác +** rồi **Tạo lịch mới**.
+3. Thêm tên lịch, **HK211** chẳng hạn.
+4. Nhấp vào **Tạo lịch**.
+
+## Nhập file .ics vào lịch mới tạo
+
+Sau khi tạo lịch:
+
+1. Ở bên trái, hãy nhấp vào **Nhập và xuất**.
+2. Nhấp vào **Chọn tệp từ máy tính** rồi chọn file vừa tải về.
+3. Chọn lịch mà bạn vừa tạo ở bước trên.
+4. Nhấp vào **Nhập**.
+
+Vậy là xong rồi á!
+
+___
+
+## Bonus: Bật thông báo cho lịch.
+
+1. Ở bên trái, hãy nhấp vào lịch mà bạn vừa tạo.
+2. Kéo xuống và tìm chỗ **Thông báo sự kiện**, nhập vào **Thêm thông báo**
+3. Chỉnh thời gian lại như ý muốn là được!
+
+Sau khi làm xong, trước mỗi giờ học, app Google Calendar trên điện thoại của bạn sẽ thông báo.
+
+## Bonus 2: Thêm link Google Meet
+
+Nếu bạn sử dụng mail trường thì sẽ có thêm một tính năng thêm link Google Meet. Thêm như sau:
+
+1. Nhấp vào từng sự kiện rồi nhấp vào cây bút **Chỉnh sửa sự kiện**.
+2. Nhấp vào cái nút màu xanh **Thêm hội nghị truyền hình trên Google Meet**.
+3. Nhấp vào mũi tên chỉ xuống kế bên bánh răng.
+4. Rê chuột vào kế bên link Meet, nhấp vào cây bút để chỉnh sửa link thành link meet của thầy cô các bạn.
+
+Còn không, các bạn thêm link Meet vào ô địa điểm hay mô tả cũng được.
+
+___
+
+Made with love by NDK ❤️
+""" ]
+
+
+
+-- Html.div []
+--     [ Html.h2 [] [ Html.text "Tạo lịch mới trên Google Calendar" ]
+--     , Html.p []
+--         [ Html.text "Đọc hướng dẫn tại đây: "
+--         , Html.a [ href "https://support.google.com/calendar/answer/37095" ] [ Html.text "Tạo lịch mới" ]
+--         ]
+--     , Html.p [] [ Html.text "Bạn hãy tạo một cái lịch tên là HK211, chẳng hạn." ]
+--     , Html.h2 [] [ Html.text "Nhập file .ics vào lịch mới tạo" ]
+--     , Html.p []
+--         [ Html.text "Đọc hướng dẫn tại đây (bước 2): "
+--         , Html.a [ href "https://support.google.com/calendar/answer/37118" ] [ Html.text "Nhập các sự kiện vào Lịch Google" ]
+--         ]
+--     , Html.p [] [ Html.text "Bạn hãy nhập file vừa tải về vào lịch mới tạo nhé." ]
+--     , Html.h2 [] [ Html.text "Bonus: Bật thông báo cho lịch" ]
+--     , Html.p [] [ Html.text ""]
+--     ]
+
+
+viewDownload : Array Event -> Html Msg
+viewDownload events =
     let
         downloadLink =
             "data:text/calendar," ++ percentEncode (Array.toList events |> Event.toCalendar)
     in
     Html.div []
-        [ Html.a [ target "_blank", href downloadLink, download "export" ] [ Html.text "Tải về" ]
-        , Html.pre [] [ Html.text (Array.toList events |> Event.toCalendar) ]
+        [ Html.p [] [ Html.a [ target "_blank", href downloadLink, download "export" ] [ Html.text "Tải về" ] ]
+        , Html.p [] [ Html.text "Nếu bạn chưa rõ tải về rồi làm gì, đọc tiếp hướng dẫn bên dưới." ]
         ]
-
-
-viewTimetable : Timetable -> Html Msg
-viewTimetable timetable =
-    Html.p []
-        [ Html.text <| "Học kỳ " ++ String.fromInt (timetable.from |> remainderBy 100 |> (*) 10 |> (+) timetable.semester)
-        , Html.button [ onClick (Convert timetable) ] [ Html.text "Convert" ]
-        ]
-
-
-
--- layout [] <|
---     column
---         [ height fill
---         , width fill
---         , Font.family
---             [ Font.typeface "Roboto Condensed"
---             , Font.sansSerif
---             ]
---         ]
---     <|
---         [ row
---             [ height (px 50)
---             , width fill
---             , spacing 10
---             , paddingXY 20 0
---             , Bg.color black
---             , Font.color white
---             ]
---             [ el [] <| text "BKalendar"
---             , el [ alignRight ] <| text "Home"
---             , el [ alignRight ] <| text "Bonus"
---             , el [ alignRight ] <| text "About"
---             ]
---         , row
---             [ height (px 400)
---             , width fill
---             , paddingXY 100 20
---             , spacing 50
---             , Bg.color gutterGrey
---             , Font.color white
---             ]
---             [ paragraph
---                 [ width (fillPortion 2)
---                 , Font.size 48
---                 , Font.light
---                 ]
---               <|
---                 [ text "Chuyển từ lịch "
---                 , el [ Font.color blue ] <| text "MyBK "
---                 , text "sang "
---                 , el [ Font.color blue ] <| text "G"
---                 , el [ Font.color darkRed ] <| text "o"
---                 , el [ Font.color darkYellow ] <| text "o"
---                 , el [ Font.color blue ] <| text "g"
---                 , el [ Font.color green ] <| text "l"
---                 , el [ Font.color darkRed ] <| text "e "
---                 , text " Calendar chỉ với vài thao tác!"
---                 ]
---             , column [ width (fillPortion 1), spacing 20 ]
---                 [ Input.multiline
---                     [ height (px 150)
---                     , Bg.color commentGrey
---                     , Border.width 0
---                     ]
---                     { onChange = GotInput
---                     , text = model.raw
---                     , placeholder = Nothing
---                     , label = Input.labelAbove [] <| text "paste lịch vào đây"
---                     , spellcheck = False
---                     }
---                 , if model.readyToDownload then
---                     downloadAs [ Font.color green ]
---                         { label =
---                             el [ Font.bold ] <|
---                                 text "➥ click để tải về"
---                         , filename = "calendar"
---                         , url = "data:text/calendar," ++ percentEncode (Class.toCalendar model.events)
---                         }
---                   else
---                     el [ Font.color lightRed ] <| text "kiểm tra lại nhá"
---                 ]
---             ]
---         , row
---             [ width fill, paddingXY 50 20, spacingXY 50 0 ]
---             [ column [ Font.center, width (fillPortion 1), spacingXY 0 10, alignTop ]
---                 [ el [ width fill, Font.bold ] <| text "bước 1"
---                 , paragraph []
---                     [ text "copy thời khóa biểu của bạn trên MyBK" ]
---                 , paragraph [ Font.size 14, Font.italic ]
---                     [ text "(pro-tip: Ctrl + A cả trang cũng được, nhớ chọn học kỳ 2)" ]
---                 , image [ centerX ] { description = "tutorial", src = "./public/tutorial.gif" }
---                 ]
---             , column [ Font.center, width (fillPortion 1), spacingXY 0 10, alignTop ]
---                 [ el [ width fill, Font.bold ] <| text "bước 2"
---                 , paragraph []
---                     [ text "paste vào ô phía phải trên, rồi tải file .ics về" ]
---                 ]
---             , column [ Font.center, width (fillPortion 1), spacingXY 0 10, alignTop ]
---                 [ el [ width fill, Font.bold ] <| text "bước 3"
---                 , paragraph []
---                     [ text <|
---                         "tạo một lịch mới trong Google Calendar, "
---                             ++ "sau đó import file vừa mới tải về vào lịch đó"
---                     ]
---                 ]
---             ]
---         , paragraph
---             [ paddingXY 0 5, alignBottom, Font.center ]
---             [ image [ height (px 16) ]
---                 { description = "logo facebook", src = "./public/facebook.svg" }
---             , link [] { label = text " facebook", url = "https://www.facebook.com/dykhng/" }
---             , text " · "
---             , image [ height (px 16) ]
---                 { description = "logo github", src = "./public/github.svg" }
---             , link [] { label = text " github", url = "https://github.com/iceghost/bkalendar" }
---             ]
---         ]
 
 
 type Msg
     = GotInput String
-    | Convert Timetable
-    | GotEvent Event
+    | GotEvent Int Event
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -199,23 +153,25 @@ update msg model =
     case msg of
         GotInput raw ->
             let
-                newResult =
+                parseResult =
                     Timetable.parse raw
             in
+            ( { model | raw = raw, readyToDownload = False, events = Array.empty }
+            , case parseResult of
+                Nothing ->
+                    Cmd.none
+
+                Just timetable ->
+                    timetableToEvents (GotEvent (List.length timetable.entries)) timetable
+            )
+
+        GotEvent total event ->
             ( { model
-                | raw = raw
-                , result = newResult
+                | events = Array.push event model.events
+                , readyToDownload = Array.length model.events + 1 == total
               }
             , Cmd.none
             )
-
-        Convert timetable ->
-            ( { model | events = Array.empty }
-            , timetableToEvents GotEvent timetable
-            )
-
-        GotEvent event ->
-            ( { model | events = Array.push event model.events }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
