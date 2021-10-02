41f98b209070f830f04dfa0e53e69a381c0048cc
Duy Khang <khangemail22@gmail.com>
2021-10-02 18:11:33 +0700

    Fix icon color

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -19,7 +19,7 @@
           <!-- prettier-ignore -->
           <svg class="w-5 h-5 light:text-gray-700" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 30 30" enable-background="new 0 0 30 30" xml:space="preserve">
             <circle fill="currentColor" cx="15" cy="15" r="15"/>
-            <path class="dark:text-blue-deep" fill="currentColor" d="M16.4,23.9v-8.1h2.7l0.4-3.2h-3.1v-2c0-0.9,0.3-1.5,1.6-1.5l1.7,0V6.2c-0.3,0-1.3-0.1-2.4-0.1  c-2.4,0-4.1,1.5-4.1,4.2v2.3h-2.7v3.2h2.7v8.1H16.4z"/>
+            <path class="text-gray-50 dark:text-blue-deep" fill="currentColor" d="M16.4,23.9v-8.1h2.7l0.4-3.2h-3.1v-2c0-0.9,0.3-1.5,1.6-1.5l1.7,0V6.2c-0.3,0-1.3-0.1-2.4-0.1  c-2.4,0-4.1,1.5-4.1,4.2v2.3h-2.7v3.2h2.7v8.1H16.4z"/>
           </svg>
         </a>
       </li>
